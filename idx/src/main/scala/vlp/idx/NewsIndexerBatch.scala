43bd3304d4b3c72ebdf8b5c7472f617e824158e4
phuonglh <phuonglh@gmail.com>
2020-05-15 23:52:36 +0700

    Update idx module

--- idx/src/main/scala/vlp/idx/NewsIndexerBatch.scala
+++ idx/src/main/scala/vlp/idx/NewsIndexerBatch.scala
@@ -16,22 +16,29 @@ import java.nio.file.{Files, Paths}
   * Reads the MySQL database of URLs, extracts their contents and saves them to a file.
   */
 object NewsIndexerBatch {
+  final val batchSize = 20000
+
   def main(args: Array[String]): Unit = {
     val urls = MySQL.getURLs
     System.setProperty("http.agent", "Chrome")
     System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2")
     println(s"#(totalURLs) = ${urls.size}")
-    val news = urls.par.map(url => {
-      println(url)
-      val content = NewsIndexer.extract(url)
-      new News(url, content, new Date())
-    }).toList
-    val accept = (s: String) => (s.size >= 500 && !s.contains("<div") && !s.contains("<table") && !s.contains("</p>"))
-    val ns = news.filter(x => accept(x.getContent()))
-    println(s"#(totalNews) = ${ns.size}")
-    implicit val formats = DefaultFormats
-    implicit val f = Serialization.formats(NoTypeHints)
-    val xs = ns.par.map(e => Serialization.write(e)).toList
-    Files.write(Paths.get("dat/news/all.json"), xs, StandardCharsets.UTF_8)
+    val batches = urls.sliding(batchSize)
+    var i = 1
+    for (batch <- batches) {
+      println(batch.size)
+      val news = batch.par.filterNot(_.contains("bbc.com")).map(url => {
+        val content = NewsIndexer.extract(url)
+        new News(url, content, new Date())
+      }).toList
+      val accept = (s: String) => (s.size >= 500 && !s.contains("<div") && !s.contains("<table") && !s.contains("</p>"))
+      val ns = news.filter(x => accept(x.getContent()))
+      println(s"#(batchSize) = ${ns.size}")
+      implicit val formats = DefaultFormats
+      implicit val f = Serialization.formats(NoTypeHints)
+      val xs = ns.par.map(e => Serialization.write(e)).toList
+      Files.write(Paths.get("dat/txt/" + i + ".json"), xs, StandardCharsets.UTF_8)
+      i = i + 1
+    }
   }
 }
