6b0cc27331dfe3e26d55a554e194e1b8964dd3c3
phuonglh <phuonglh@gmail.com>
2020-07-03 21:08:53 +0700

    Update idx module

--- idx/src/main/scala/vlp/idx/NewsExtractorBatch.scala
+++ idx/src/main/scala/vlp/idx/NewsExtractorBatch.scala
@@ -15,7 +15,7 @@ import java.nio.file.{Files, Paths}
   * 
   * Reads the MySQL database of URLs, extracts their contents and saves them to a file.
   */
-object NewsIndexerBatch {
+object NewsExtractorBatch {
   final val batchSize = 10000
 
   case class News(url: String, content: String)
@@ -26,22 +26,27 @@ object NewsIndexerBatch {
     System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2")
     println(s"#(totalURLs) = ${urls.size}")
     val outputPath = if (args(0).nonEmpty) { if (args(0) endsWith "/") args(0) else args(0) + "/" } else ""
-    val batches = urls.sliding(batchSize)
+    println(s"Sliding with size ${batchSize}")
+    val batches = urls.sliding(batchSize, batchSize).toList
+    println("#(batches) = " + batches.size)
     var i = 1
     for (batch <- batches) {
       println(batch.size)
-      val news = batch.par.filterNot(u => u.contains("bbc.com") || u.contains("baohaiquan.vn")).map(url => {
-        val content = NewsIndexer.extract(url)
-        News(url, content)
-      }).toList
+      val news = batch.par.filterNot(u => u.contains("bbc.com") || u.contains("baohaiquan.vn") || u.contains("vneconomy.vn"))
+        .map(url => { 
+          val content = NewsIndexer.extract(url)
+          News(url, content)
+        }).toList
       val accept = (s: String) => (s.size >= 500 && !s.contains("<div") && !s.contains("<table") && !s.contains("</p>"))
       val ns = news.filter(x => accept(x.content))
-      println(s"#(batchSize) = ${ns.size}")
+      println(s"#(this batch size) = ${ns.size}")
       implicit val formats = DefaultFormats
       implicit val f = Serialization.formats(NoTypeHints)
       val xs = ns.par.map(e => Serialization.write(e)).toList
+      println("Writing extraction result...")
       Files.write(Paths.get(outputPath + i + ".json"), xs, StandardCharsets.UTF_8)
       i = i + 1
     }
+    println("Done.")
   }
 }
