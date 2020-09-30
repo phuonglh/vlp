bda552ecfc96a1a80945c65b508be4ff183317f1
phuonglh <phuonglh@gmail.com>
2020-09-30 23:26:31 +0700

    Update ner module

--- ner/src/main/scala/vlp/ner/Utils.scala
+++ ner/src/main/scala/vlp/ner/Utils.scala
@@ -8,6 +8,10 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import java.io.IOException
 import java.nio.file.SimpleFileVisitor
+import java.util.stream.Collectors
+import scala.io.Source
+import java.io.File
+import java.nio.charset.StandardCharsets
 
 object Utils {
 
@@ -51,7 +55,7 @@ object Utils {
     * @param inputPath
     * @param outputPath
     */
-  def convert(inputPath: String, outputPath: String): Unit = {
+  def convertHUS(inputPath: String, outputPath: String): Unit = {
     val files = Files.walkFileTree(Paths.get(inputPath), new SimpleFileVisitor[Path]() {
       override def visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult = {
         val currentFileName = file.getFileName().toString()
@@ -65,8 +69,54 @@ object Utils {
     })
   }
 
+  /**
+    * Converts the XML format of BPO annotated files into 2-col with B-I-O format.
+    *
+    * @param inputPath
+    * @param outputPath
+    */
+  def convertBPO(inputPath: String, outputPath: String): Unit = {
+    val entityMap = Map[String, String]("cq" -> "ORG", "dd" -> "LOC", "ng" -> "PER", "vb" -> "DOC")
+
+    def xml2Column(element: scala.xml.Elem): List[String] = {
+      val tokens = element.child.flatMap(node => {
+        if (node.getClass().getName().contains("Elem")) {
+          val entityType = entityMap.getOrElse(node.label, "UNK")
+          val tokens = if (entityType != "DOC") vlp.tok.Tokenizer.tokenize(node.text.trim).map(_._3) else node.text.trim.split("\\s+").toList
+          Array(tokens.head + " " + "B-" + entityType) ++ tokens.tail.map(word => word + " " + "I-" + entityType)
+        } else {
+          val tokens = vlp.tok.Tokenizer.tokenize(node.text.trim).map(_._3)
+          tokens.map(token => token + " " + "O")
+        }
+      })
+      tokens.toList
+    }
+
+    // get all files in the data path
+    import scala.collection.JavaConversions._
+    val paths = Files.walk(Paths.get(inputPath)).collect(Collectors.toList()).filter(path => Files.isRegularFile(path))
+    println(paths.size)
+    // read all lines of these files and collect them into a list of lines
+    val lines = paths.flatMap(path => {
+      try {
+        Source.fromFile(new File(path.toString()), "utf-8").getLines().map(_.trim()).filter(_.nonEmpty).toList
+      } catch {
+        case e: Exception => { println(path.toString); List.empty }
+      }
+    })
+    val elements = lines.toList.map(line => {
+      // println(line)
+      scala.xml.XML.loadString("<s>" + line.replaceAll("&", "&amp;") + "</s>")
+    })
+    val texts = elements.map(element => xml2Column(element).mkString("\n") + "\n")
+    Files.write(Paths.get(outputPath), texts, StandardCharsets.UTF_8)
+
+  }
+
   def main(args: Array[String]): Unit = {
     // rename("/Users/phuonglh/Downloads/annotation-2/", "/Users/phuonglh/vlp/dat/ner/stm")
-    convert("/Users/phuonglh/vlp/dat/ner/stm", "/Users/phuonglh/vlp/dat/ner/lad/")
+    // convertHUS("/Users/phuonglh/vlp/dat/ner/stm", "/Users/phuonglh/vlp/dat/ner/lad/")
+    convertBPO("/Users/phuonglh/vlp/dat/ner/bpo/man", "/Users/phuonglh/vlp/dat/ner/bpo/man.tsv")
+    println("Done.")
   }
 }
