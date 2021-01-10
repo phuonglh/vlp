37e71d64c56e7353916286d69fecf32ce1f77540
phuonglh <phuonglh@gmail.com>
2021-01-10 23:15:43 +0700

    Update aep module

--- jul/aep/src/Eval.jl
+++ jul/aep/src/Eval.jl
@@ -6,51 +6,58 @@ using JSON3
 
 include("Parser.jl")
 
-times = 1
+"""
+    experiment(options, times=3)
 
-scorePath = options[:scorePath]
-file = if isfile(scorePath)
-    open(scorePath, "a")
-else
-    open(scorePath, "w")
+    Perform experimentation with a given number of times.
+"""
+function experiment(options, times=3)
+    scorePath = options[:scorePath]
+    file = if isfile(scorePath)
+        open(scorePath, "a")
+    else
+        open(scorePath, "w")
+    end
+
+    # Vietnamese
+    for t = 1:times
+        local elapsedTime = time_ns()
+        train(options)
+        elapsedTime = time_ns() - elapsedTime
+        sentencesTrain = readCorpus(options[:trainCorpus], options[:maxSequenceLength])
+        sentencesDev = readCorpus(options[:devCorpus], options[:maxSequenceLength])
+        sentencesTest = readCorpus(options[:testCorpus], options[:maxSequenceLength])
+        accuracyTrain = eval(options, sentencesTrain)
+        accuracyDev = eval(options, sentencesDev)
+        accuracyTest = eval(options, sentencesTest)
+        trainingUAS, trainingLAS = evaluate(options, sentencesTrain)
+        devUAS, devLAS = evaluate(options, sentencesDev)
+        testUAS, testLAS = evaluate(options, sentencesTest)
+        local scores = Dict{Symbol,Any}(
+            :trainCorpus => options[:trainCorpus],
+            :minFreq => options[:minFreq],
+            :maxSequenceLength => options[:maxSequenceLength],
+            :wordSize => options[:wordSize],
+            :shapeSize => options[:shapeSize],
+            :posSize => options[:posSize], 
+            :embeddingSize => options[:embeddingSize],
+            :hiddenSize => options[:hiddenSize],
+            :batchSize => options[:batchSize],
+            :trainingTime => elapsedTime,
+            :trainingAccuracy => accuracyTrain,
+            :developmentAccuracy => accuracyDev,
+            :testAccuracy => accuracyTest,
+            :trainingUAS => trainingUAS,
+            :trainingLAS => trainingLAS,
+            :devUAS => devUAS,
+            :devLAS => devLAS,
+            :testUAS => testUAS,
+            :testLAS => testLAS
+        )
+        line = JSON3.write(scores)
+        write(file, string(line, "\n"))
+        flush(file)
+    end
+    close(file)
 end
 
-# Vietnamese
-for t = 1:times
-    local elapsedTime = time_ns()
-    train(options)
-    elapsedTime = time_ns() - elapsedTime
-    sentencesTrain = readCorpus(options[:trainCorpus], options[:maxSequenceLength])
-    sentencesDev = readCorpus(options[:devCorpus], options[:maxSequenceLength])
-    sentencesTest = readCorpus(options[:testCorpus], options[:maxSequenceLength])
-    accuracyTrain = eval(options, sentencesTrain)
-    accuracyDev = eval(options, sentencesDev)
-    accuracyTest = eval(options, sentencesTest)
-    trainingUAS, trainingLAS = evaluate(options, sentencesTrain)
-    devUAS, devLAS = evaluate(options, sentencesDev)
-    testUAS, testLAS = evaluate(options, sentencesTest)
-    local scores = Dict{Symbol,Any}(
-        :trainCorpus => options[:trainCorpus],
-        :minFreq => options[:minFreq],
-        :maxSequenceLength => options[:maxSequenceLength],
-        :wordSize => options[:wordSize],
-        :shapeSize => options[:shapeSize],
-        :posSize => options[:posSize], 
-        :embeddingSize => options[:embeddingSize],
-        :hiddenSize => options[:hiddenSize],
-        :batchSize => options[:batchSize],
-        :trainingTime => elapsedTime,
-        :trainingAccuracy => accuracyTrain,
-        :developmentAccuracy => accuracyDev,
-        :testAccuracy => accuracyTest,
-        :trainingUAS => trainingUAS,
-        :trainingLAS => trainingLAS,
-        :devUAS => devUAS,
-        :devLAS => devLAS,
-        :testUAS => testUAS,
-        :testLAS => testLAS
-    )
-    line = JSON3.write(scores)
-    write(file, string(line, "\n"))
-end
-close(file)
