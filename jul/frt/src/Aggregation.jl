495736927e838bea9e4cdbcdd6ad374d44388b81
phuonglh <phuonglh@gmail.com>
2021-01-14 13:54:02 +0700

    Update frt module

--- jul/frt/src/Aggregation.jl
+++ jul/frt/src/Aggregation.jl
@@ -34,15 +34,15 @@ end
 
 # ORDR
 
-- docentry: so don hang
-- doctype: loai don hang (01: Ban tai quay; 02: Ban tra gop;...)
-- docstatus: trang thai (A: Đã duyệt, C: Hủy, D: Hoàn tất thu cọc, F: Hoàn tất, N: Chờ xử lý, O: Mở, S: Lưu, T: Đã trả hàng, W: Đang trả hàng)
-- ubus_lin: kenh ban hang
-- u_shpcod: cua hang
-- u_tmonpr: tong tien chua thue
-- u_exdate: ngay doi hang
-- u_desc: ghi chu
-- u_tmontx: tong tien thue
-- u_tmonbi: tong tien phai thu
-- type_returnso: kieu don hang
-- docdate: ngay ban
+# - docentry: so don hang
+# - doctype: loai don hang (01: Ban tai quay; 02: Ban tra gop;...)
+# - docstatus: trang thai (A: Đã duyệt, C: Hủy, D: Hoàn tất thu cọc, F: Hoàn tất, N: Chờ xử lý, O: Mở, S: Lưu, T: Đã trả hàng, W: Đang trả hàng)
+# - ubus_lin: kenh ban hang
+# - u_shpcod: cua hang
+# - u_tmonpr: tong tien chua thue
+# - u_exdate: ngay doi hang
+# - u_desc: ghi chu
+# - u_tmontx: tong tien thue
+# - u_tmonbi: tong tien phai thu
+# - type_returnso: kieu don hang
+# - docdate: ngay ban
