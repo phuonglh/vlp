b3f47b748e1db9c4a871c35a283f19136edfbe8c
phuonglh <phuonglh@gmail.com>
2021-01-17 16:30:44 +0700

    Update frt module

--- jul/frt/src/Analysis.jl
+++ jul/frt/src/Analysis.jl
@@ -1,7 +1,9 @@
 # phuonglh@gmail.com
-using StatsPlots
 
-include("DataReader.jl")
+using DataFrames
+using CSV
+using StatsPlots
+using Statistics
 
 
 """
@@ -14,7 +16,7 @@ function analyseOrder(df)
     @info "Analysing the order df..."
     # select the orders with status "F" (complete); there are 347,258 complete records in January 2018
     # filter out 2 warehouses (11000 and 11001)
-    complete = df[(df.status .== "F") .& (df.shop .!= "11000") .& (df.shop .!= "11001"), :]
+    complete = df[(df.status .== "F") .& (df.shop .!= 11000) .& (df.shop .!= 11001), :]
     # group the complete df by shop, there are 516 shops in January 2018
     gdf = groupby(complete, :shop)
     # count the number of orders for each shop 
@@ -27,19 +29,13 @@ function analyseOrder(df)
 
     # analyse the top-performing shop
     topShop = gdf[first(topTen)]
-    # transform the `amount` column to numeric type and `date` column to date type
-    ef = select(topShop, 
-        :amount => (x -> parse.(Float32, x) ./ 10^6) => :amount, 
-        :date => (x -> parse.(Date, map(d -> d[1:10], x))) => :date,
-        :entry => :entry
-    )
-    # group ef by date
-    efByDate = groupby(ef, :date)
+    # group by date
+    efByDate = groupby(topShop, :date)
     # compute the total amount for each date
     sale = combine(efByDate, :amount => sum => :sale)
     sort!(sale, :date)
     sdf = select(sale, :date => (x -> Dates.day.(x)) => :day, :date => (x -> Dates.dayname.(x)) => :dayname, :sale)
-    return (ef, sdf, topShop[1,1])
+    return (topShop, sdf, topShop[1,1])
 end
 
 """
@@ -48,44 +44,33 @@ end
     Analyse the sale of two top items given order detail information of a month `df`.
 """
 function analyseOrderDetail(df, minPrice = 5.)
-    # remove all rows having a missing value
-    ef = dropmissing!(df)
-    # remove all rows which does not have a good (8-character length) shop code or start with 1100 (warehouses)
-    withValidShop = ef[length.(ef.shop) .== 8 .| startswith.(ef.shop, "1100"), :] 
-    # select and transform columns
-    ff = select(withValidShop, 
-        :item => :item,
-        :desc => :desc,
-        :entry => :entry,
-        :shop => (x -> map(v -> v[1:5], x)) => :shop,
-        :price => (x -> parse.(Float32, x) ./ 10^6) => :price, 
-        :quantity => (x -> Int.(parse.(Float32, x))) => :quantity
-    )
     # remove all rows whose amount are less than minAmount (milion VND)
-    gf = ff[ff.price .> minPrice, :]
+    ef = df[df.price .> minPrice, :]
     # group rows by item 
-    gdf = groupby(gf, :item) 
+    gf = groupby(ef, :item) 
     # count the number of sale for each item
-    cs = [(i, sum(gdf[i][:,:quantity])) for i = 1:length(gdf)]
+    cs = [(i, sum(gf[i][:,:qty])) for i = 1:length(gf)]
     # sort by number of orders in descending order
     sort!(cs, by = p -> p[2], rev = true)
-    top5 = gdf[map(p -> p[1], cs[1:5])]
+    top5 = gf[map(p -> p[1], cs[1:5])]
 end
 
 function analyse(month::String="01", year::String="2018")
-    orderPath = string(pwd(), "/dat/frt/ordr", month, year, ".json")
-    orderDF = readOrderDF(orderPath)
+    orderPath = string(pwd(), "/jul/frt/dat/o", year, month, ".csv")
+    orderDF = CSV.File(orderPath) |> DataFrame
     ef, sdf, topShop = analyseOrder(orderDF)
 
-    detailPath = string(pwd(), "/dat/frt/rdr1", month, year, ".json")
-    detailDF = readOrderDetailDF(detailPath)
+    detailPath = string(pwd(), "/jul/frt/dat/r", year, month, ".csv")
+    detailDF = CSV.File(detailPath) |> DataFrame
     top5 = analyseOrderDetail(detailDF)
-    # first-rank item
-    df = innerjoin(ef, top5[3], on = :entry)
+
+    ff = select(ef, [:doc, :date, :status])
+    # join with third-rank item
+    gf = innerjoin(ff, top5[3], on = :doc)
     # group by date
-    byDate = groupby(df, :date)
+    byDate = groupby(gf, :date)
     # compute the total quantity for each date
-    sale = combine(byDate, :quantity => sum => :sale)
+    sale = combine(byDate, :price => mean => :price, :disc => mean => :disc, :qty => sum => :quantity)
     sort!(sale, :date)
     return (topShop, topItem[1,1], sale)
 end
--- jul/frt/src/DataReader.jl
+++ jul/frt/src/DataReader.jl
@@ -7,10 +7,11 @@ using Dates
 using FLoops
 using BangBang
 using MicroCollections
+using CSV
 
 # All fields of orderDF are Set([:docstatus, :u_buslin, :type_returnso, :u_tmonpr, :u_exdate, :doctype, :u_tmonbi, :docdate, :docentry, :u_tmontx, :u_shpcod, :u_desc])
-# We are interested in a number of selected orders.
-orderFields = Dict(:u_shpcod => :shop, :u_tmonbi => :amount, :docstatus => :status, :docdate => :date, :docentry => :entry)
+# We are interested in 5 selected orders.
+orderFields = Dict(:u_shpcod => :shop, :u_tmonbi => :amount, :docstatus => :status, :docdate => :date, :docentry => :doc)
 
 """
     readOrderDF(path, numRecords)
@@ -26,7 +27,7 @@ function readOrderDF(path::String, numRecords::Int=-1)::DataFrame
         lines
     end
     # collect all objects and fields
-    @info "Reading order objects..."
+    @info "Reading order objects from $(path)..."
     objects = map(line -> JSON3.read(line), xs)
     @info "Joining strings"
     s = string("[", join(xs, ","), "]");
@@ -41,12 +42,23 @@ function readOrderDF(path::String, numRecords::Int=-1)::DataFrame
     data = Dict(entries)
     # convert to a data frame, if a field is not present then it is marked as `missing`
     @info "Preparing df..."
-    DataFrame(data)
+    df = DataFrame(data)
+    # transform the df
+    dropmissing!(df)
+    ef = select(df, 
+        :shop => :shop,
+        :amount => (x -> parse.(Float32, x) ./ 10^6) => :amount, 
+        :status => :status,
+        :date => (x -> parse.(Date, map(d -> d[1:10], x))) => :date,
+        :doc => :doc
+    )
+    return ef
 end
 
 # Selected fields of an order detail data frame.
-orderDetailFields = Dict(:itemcode => :item, :dscription => :desc, :quantity => :quantity, :discprcnt => :discount, 
-    :u_tmoney => :price, :whscode => :shop, :docentry => :entry)
+orderDetailFields = Dict(:itemcode => :item, :dscription => :desc, :quantity => :qty, :discprcnt => :disc, 
+    :u_tmoney => :price, :whscode => :shop, :docentry => :doc)
+# it turns out that :disc is not in percentage unit, it is the same unit as :price.
 
 """
     readOrderDetailDF(path, numRecords)
@@ -62,7 +74,7 @@ function readOrderDetailDF(path::String, numRecords::Int=-1)::DataFrame
         lines
     end
     # collect all objects and fields
-    @info "Reading order detail objects..."
+    @info "Reading order detail objects from $(path)..."
     objects = map(line -> JSON3.read(line), xs)
     @info "Joining strings"
     s = string("[", join(xs, ","), "]");
@@ -77,5 +89,40 @@ function readOrderDetailDF(path::String, numRecords::Int=-1)::DataFrame
     data = Dict(entries)
     # convert to a data frame, if a field is not present then it is marked as `missing`
     @info "Preparing df..."
-    DataFrame(data)
+    df = DataFrame(data)
+    # transform the df
+    dropmissing!(df)
+    # remove all rows which does not have a good (8-character length) shop code or start with 1100 (warehouses aren't treated)
+    withValidShop = df[length.(df.shop) .== 8 .| startswith.(df.shop, "1100"), :] 
+    ef = select(withValidShop, 
+        :item => :item,
+        :desc => :desc,
+        :doc => :doc,
+        :disc => (x -> parse.(Float32, x) ./ 10^6) => :disc,
+        :shop => (x -> map(v -> v[1:5], x)) => :shop,
+        :price => (x -> parse.(Float32, x) ./ 10^6) => :price, 
+        :qty => (x -> Int.(parse.(Float32, x))) => :qty
+    )
+    return ef
+end
+
+"""
+    preprocess(year)
+
+    Read all order data and order detail data into data frames, preprocess them and write
+    backs the DFs to JSON files for later processing.
+"""
+function preprocess(year::String="2018")
+    months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+    for month in months
+        input = string(pwd(), "/dat/frt/ordr", month, year, ".json")
+        orderDF = readOrderDF(input)
+        output = string(pwd(), "/jul/frt/dat/o", year, month, ".csv")
+        CSV.write(output, orderDF)
+
+        input = string(pwd(), "/dat/frt/rdr1", month, year, ".json")
+        detailDF = readOrderDetailDF(input)
+        output = string(pwd(), "/jul/frt/dat/r", year, month, ".csv")
+        CSV.write(output, detailDF)
+    end
 end
