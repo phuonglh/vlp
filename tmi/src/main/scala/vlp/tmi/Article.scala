7d9a4d74300f40844fc983681b7c9865385e9127
phuonglh <phuonglh@gmail.com>
2021-03-28 08:51:47 +0700

    Add GDELT service

---
+++ tmi/src/main/scala/vlp/tmi/Article.scala
@@ -0,0 +1,26 @@
+package vlp.tmi
+
+/**
+  * GDELT article structure.
+  * 
+  * phuonglh@gmail.com
+  *
+  * @param url
+  * @param url_mobile
+  * @param title
+  * @param seendate
+  * @param socialimage
+  * @param domain
+  * @param language
+  * @param sourcecountry
+  */
+case class Article(
+    url: String,
+    url_mobile: String,
+    title: String,
+    seendate: String,
+    socialimage: String,
+    domain: String,
+    language: String,
+    sourcecountry: String
+)
---
+++ tmi/src/main/scala/vlp/tmi/GDELT.scala
@@ -0,0 +1,46 @@
+package vlp.tmi
+
+import org.json4s.DefaultFormats
+import org.json4s._
+import org.json4s.jackson.JsonMethods.parse
+
+import scala.collection.mutable.ListBuffer
+import scala.io.Source
+
+import scalaj.http._
+
+/**
+ * An utility to query news from the GDELT Project through its Summary API.
+ * 
+ * phuonglh@gmail.com, March 2021.
+ * 
+ */
+object GDELT {
+    val summaryApiPrefix = "http://api.gdeltproject.org/api/v2/doc/doc?query="
+    val summaryApiSuffix = "%20sourcecountry:vietnam&mode=artlist&timespan=1day&sort=datedesc&format=json"
+
+    /**
+      * Submits a query to the GDELT API and get a list of articles.
+      *
+      * @param keyword
+      * @return a list of articles.
+      */
+    def runService(keyword: String): List[Article] = {
+        val summaryApi = summaryApiPrefix + keyword + summaryApiSuffix
+        val response: HttpResponse[String] = Http(summaryApi).asString
+        implicit val formats = DefaultFormats
+        val jsArray = parse(response.body)
+        val result = ListBuffer[Article]()
+        for (articles <- jsArray.children) {
+            for (article <- articles.children)
+                result += article.extract[Article]
+        }
+        result.toList
+    }
+
+    def main(args: Array[String]): Unit = {
+        val result = runService("environment")
+        println(result.size)
+        result.foreach(println)
+    }
+}
