586325b8872d5f168e30e26967a31e74ec0044b8
phuonglh <phuonglh@gmail.com>
2022-03-22 22:36:43 +0700

    Update btc

--- btc/src/main/java/BlockChain.java
+++ btc/src/main/java/BlockChain.java
@@ -1,23 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 // Block Chain should maintain only limited block nodes to satisfy the functions
 // You should not have all the blocks added to the block chain in memory 
 // as it would cause a memory overflow.
 
 public class BlockChain {
-    public static final int CUT_OFF_AGE = 10;
+    public static final int CUT_OFF_AGE = 2;
     class Node {
         Block block;
         Node parent;
         List<Node> children;
-        Node(Block block) {
+        Node(Block block, Node parent) {
             this.block = block;
-            this.parent = null;
+            this.parent = parent;
             this.children = new LinkedList<>();
+        }
+        Node(Block block) {
+            this(block, null);
         }
     }
     Node chain;
+    Map<Block, UTXOPool> uPoolMap = new HashMap<>();
+    TransactionPool txPool = new TransactionPool();
 
     /**
      * create an empty block chain with just a genesis block. Assume {@code genesisBlock} is a valid
@@ -26,6 +35,7 @@ public class BlockChain {
     public BlockChain(Block genesisBlock) {
         // IMPLEMENT THIS
         chain = new Node(genesisBlock);
+        uPoolMap.put(genesisBlock, new UTXOPool());
     }
 
     class Pair {
@@ -43,7 +53,7 @@ public class BlockChain {
         else {
             Pair[] pairs = new Pair[node.children.size()];
             int maxHeightIndex = 0;
-            for (int j = 1; j < pairs.length; j++) {
+            for (int j = 0; j < pairs.length; j++) {
                 pairs[j] = getMaxPair(node.children.get(j));
                 if (pairs[maxHeightIndex].height < pairs[j].height) {
                     maxHeightIndex = j;
@@ -62,13 +72,16 @@ public class BlockChain {
     /** Get the UTXOPool for mining a new block on top of max height block */
     public UTXOPool getMaxHeightUTXOPool() {
         // IMPLEMENT THIS
-        return null;
+        Block block = getMaxHeightBlock();
+        if (!uPoolMap.containsKey(block))
+            uPoolMap.put(block, new UTXOPool());
+        return uPoolMap.get(block);
     }
 
     /** Get the transaction pool to mine a new block */
     public TransactionPool getTransactionPool() {
         // IMPLEMENT THIS
-        return null;
+        return txPool;
     }
 
     /**
@@ -87,12 +100,52 @@ public class BlockChain {
         // IMPLEMENT THIS
         if (block.getPrevBlockHash() == null)
             return false;
-        
-        return false;
+        // check validity
+        TxHandler txHandler = new TxHandler(getMaxHeightUTXOPool());
+        List<Transaction> txs = block.getTransactions();
+        boolean isValidBlock = true;
+        for (Transaction tx: txs)
+            if (!txHandler.isValidTx(tx)) {
+                isValidBlock = false;
+                break;
+            }
+        if (!isValidBlock)
+            return false;
+        // removed added txs from the transaction pool
+        for (Transaction tx: txs)
+            txPool.removeTransaction(tx.getHash());
+
+        // find nodes at height > maxHeight - CUT_OFF_AGE
+        int maxHeight = getMaxPair(chain).height;
+        System.out.println(maxHeight);
+        Node p = chain;
+        int height = 0;
+        List<Node> candidates = new ArrayList<>();
+        while (p != null && height <= maxHeight - CUT_OFF_AGE) {
+            candidates.add(p);
+            height++;
+            if (p.children.isEmpty()) 
+                p = null;
+            else {
+                int j = new Random().nextInt(p.children.size());
+                p = p.children.get(j);
+            }
+        }
+        // append the given block into one branch, here I deliberatly choose the last candidate
+        if (candidates.isEmpty())
+            candidates.add(chain);
+        Node last = candidates.get(candidates.size()-1);
+        last.children.add(new Node(block, last));
+        // debug: print blockchain
+        // 
+        // update the genesis block
+        chain = candidates.get(0);
+        return true;
     }
 
     /** Add a transaction to the transaction pool */
     public void addTransaction(Transaction tx) {
         // IMPLEMENT THIS
+        txPool.addTransaction(tx);
     }
 }
--- btc/src/main/java/Main.java
+++ btc/src/main/java/Main.java
@@ -13,121 +13,120 @@ import java.security.*;
 
 public class Main {
 
-    public static void main(String[] args)
-            throws NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+   public static void main(String[] args) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
 
         /*
          * Generate key pairs, for Scrooge, Alice & Bob
          */
         KeyPair pk_scrooge = KeyPairGenerator.getInstance("RSA").generateKeyPair();
-        KeyPair pk_alice = KeyPairGenerator.getInstance("RSA").generateKeyPair();
-        KeyPair pk_bob = KeyPairGenerator.getInstance("RSA").generateKeyPair();
+        KeyPair pk_alice   = KeyPairGenerator.getInstance("RSA").generateKeyPair();
+        KeyPair pk_bob     = KeyPairGenerator.getInstance("RSA").generateKeyPair();
 
-        /*
+	/*
          * Create Genesis block: No TXs, but 1 Coinbase
          */
-        Block genesis = new Block(null, pk_scrooge.getPublic());
-        genesis.finalize();
-        BlockChain bc = new BlockChain(genesis);
-        BlockHandler bh = new BlockHandler(bc);
+	Block genesis = new Block(null, pk_scrooge.getPublic());
+	genesis.finalize();
+	BlockChain bc = new BlockChain(genesis);
+	BlockHandler bh = new BlockHandler(bc);
 
-        /*
-         * Create block1 from alice with tx scrooge -> alice
+	/*
+	 * Create block1 from alice with tx scrooge -> alice
          */
-        Block block1 = new Block(genesis.getHash(), pk_alice.getPublic());
-        // new TX: scrooge pays 25 coins to alice
-        Tx tx1 = new Tx();
+	Block block1 = new Block(genesis.getHash(), pk_alice.getPublic());
+    // new TX: scrooge pays 25 coins to alice
+    Tx tx1 = new Tx();
 
-        // the genesis block has a value of 25
-        tx1.addInput(genesis.getCoinbase().getHash(), 0);
+    // the genesis block has a value of 25
+    tx1.addInput(genesis.getCoinbase().getHash(), 0);
 
-        tx1.addOutput(5, pk_alice.getPublic());
-        tx1.addOutput(10, pk_alice.getPublic());
-        tx1.addOutput(10, pk_alice.getPublic());
+    tx1.addOutput(5, pk_alice.getPublic());
+    tx1.addOutput(10, pk_alice.getPublic());
+    tx1.addOutput(10, pk_alice.getPublic());
 
-        // There is only one (at position 0) Transaction.Input in tx2
-        // and it contains the coin from Scrooge, therefore I have to sign with the
-        // private key from Scrooge
-        tx1.signTx(pk_scrooge.getPrivate(), 0);
+    // There is only one (at position 0) Transaction.Input in tx2
+    // and it contains the coin from Scrooge, therefore I have to sign with the private key from Scrooge
+    tx1.signTx(pk_scrooge.getPrivate(), 0);
 
-        block1.addTransaction(tx1);
-        block1.finalize();
+	block1.addTransaction(tx1);
+	block1.finalize();
 
-        System.out.println("Block1 Added ok: " + bh.processBlock(block1));
+	System.out.println("Block1 Added ok: " + bh.processBlock(block1));
 
-        /*
-         * Create alternative block, block2 from scrooge with tx scrooge -> scrooge
+	/*
+	 * Create alternative block, block2 from scrooge with tx scrooge -> scrooge
          */
-        Block block2 = new Block(genesis.getHash(), pk_scrooge.getPublic());
+	Block block2 = new Block(genesis.getHash(), pk_scrooge.getPublic());
 
-        // new TX: scrooge pays 25 coins to scrooge
-        Tx tx2 = new Tx();
-        tx2.addInput(genesis.getCoinbase().getHash(), 0); // 25
-        tx2.addOutput(5, pk_scrooge.getPublic());
-        tx2.addOutput(10, pk_scrooge.getPublic());
-        tx2.addOutput(10, pk_scrooge.getPublic());
-        tx2.signTx(pk_scrooge.getPrivate(), 0);
+    // new TX: scrooge pays 25 coins to scrooge
+    Tx tx2 = new Tx();
+    tx2.addInput(genesis.getCoinbase().getHash(), 0);  //25
+    tx2.addOutput(5, pk_scrooge.getPublic());
+    tx2.addOutput(10, pk_scrooge.getPublic());
+    tx2.addOutput(10, pk_scrooge.getPublic());
+    tx2.signTx(pk_scrooge.getPrivate(), 0);
 
-        block2.addTransaction(tx2);
-        block2.finalize();
+	block2.addTransaction(tx2);
+	block2.finalize();
+	
+	System.out.println("Block2 Added ok: " + bh.processBlock(block2));
 
-        System.out.println("Block2 Added ok: " + bh.processBlock(block2));
-
-        /*
-         * Create new block3 chained to block1 with tx alice -> bob
+	/*
+	 * Create new block3 chained to block1 with tx alice -> bob
          */
-        Block block3 = new Block(block1.getHash(), pk_scrooge.getPublic());
+	Block block3 = new Block(block1.getHash(), pk_scrooge.getPublic());
 
-        // new TX: alice pays 15 coins to bob
-        Tx tx3 = new Tx();
-        tx3.addOutput(20, pk_bob.getPublic());
-        tx3.addInput(tx1.getHash(), 1); // 10 coins
-        tx3.signTx(pk_alice.getPrivate(), 0);
-        tx3.addInput(tx1.getHash(), 2); // 10 coins
-        tx3.signTx(pk_alice.getPrivate(), 1);
+    // new TX: alice pays 15 coins to bob
+    Tx tx3 = new Tx();
+    tx3.addOutput(20, pk_bob.getPublic());
+    tx3.addInput(tx1.getHash(), 1);	// 10 coins
+    tx3.signTx(pk_alice.getPrivate(), 0);
+    tx3.addInput(tx1.getHash(), 2);	// 10 coins
+    tx3.signTx(pk_alice.getPrivate(), 1);
 
-        block3.addTransaction(tx3);
-        block3.finalize();
+	block3.addTransaction(tx3);
+	block3.finalize();
 
-        System.out.println("Block3 Added ok: " + bh.processBlock(block3));
+	System.out.println("Block3 Added ok: " + bh.processBlock(block3));
 
-        /*
-         * Create new block4 chained to block3 with tx bob -> bob
+	/*
+	 * Create new block4 chained to block3 with tx bob -> bob
          */
-        Block block4 = new Block(block3.getHash(), pk_scrooge.getPublic());
+	Block block4 = new Block(block3.getHash(), pk_scrooge.getPublic());
 
-        // new TX: bob splits 15 coins to bob
-        Tx tx4 = new Tx();
-        tx4.addOutput(10, pk_bob.getPublic());
-        tx4.addOutput(5, pk_bob.getPublic());
-        tx4.addInput(tx3.getHash(), 0); // 15 coins
-        tx4.signTx(pk_bob.getPrivate(), 0);
+    // new TX: bob splits 15 coins to bob
+    Tx tx4 = new Tx();
+    tx4.addOutput(10, pk_bob.getPublic());
+    tx4.addOutput(5, pk_bob.getPublic());
+    tx4.addInput(tx3.getHash(), 0);	// 15 coins
+    tx4.signTx(pk_bob.getPrivate(), 0);
 
-        block4.addTransaction(tx4);
-        block4.finalize();
+	block4.addTransaction(tx4);
+	block4.finalize();
 
-        System.out.println("Block4 Added ok: " + bh.processBlock(block4));
+	System.out.println("Block4 Added ok: " + bh.processBlock(block4));
 
-        /*
-         * Create new block5 chained to block4 with tx alice -> bob
-         */
-        Block block5 = new Block(block4.getHash(), pk_alice.getPublic());
+	/*
+	 * Create new block5 chained to block4 with tx alice -> bob
+	 */
+	Block block5 = new Block(block4.getHash(), pk_alice.getPublic());
+          
+	// new TX: alice pays 5+25 coins to bob
+    Tx tx5 = new Tx();
+    tx5.addOutput(25, pk_bob.getPublic());
+    tx5.addInput(tx1.getHash(), 0);	// 5 coins
+    tx5.signTx(pk_alice.getPrivate(), 0);
+    tx5.addInput(block1.getCoinbase().getHash(), 0); // 25 coins
+    tx5.signTx(pk_alice.getPrivate(), 1);
 
-        // new TX: alice pays 5+25 coins to bob
-        Tx tx5 = new Tx();
-        tx5.addOutput(25, pk_bob.getPublic());
-        tx5.addInput(tx1.getHash(), 0); // 5 coins
-        tx5.signTx(pk_alice.getPrivate(), 0);
-        tx5.addInput(block1.getCoinbase().getHash(), 0); // 25 coins
-        tx5.signTx(pk_alice.getPrivate(), 1);
-
-        block5.addTransaction(tx5);
-        block5.finalize();
-        System.out.println("Block5 Added ok: " + bh.processBlock(block5));
+	block5.addTransaction(tx5);
+	block5.finalize();
+	System.out.println("Block5 Added ok: " + bh.processBlock(block5));
 
     }
 
-    public static class Tx extends Transaction {
+
+    public static class Tx extends Transaction { 
         public void signTx(PrivateKey sk, int input) throws SignatureException {
             Signature sig = null;
             try {
@@ -137,9 +136,8 @@ public class Main {
             } catch (NoSuchAlgorithmException | InvalidKeyException e) {
                 throw new RuntimeException(e);
             }
-            this.addSignature(sig.sign(), input);
-            // Note that this method is incorrectly named, and should not in fact override
-            // the Java
+            this.addSignature(sig.sign(),input);
+            // Note that this method is incorrectly named, and should not in fact override the Java
             // object finalize garbage collection related method.
             this.finalize();
         }
