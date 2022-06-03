0f467f9498ac4599055c7fc0bd92ac190abd6272
phuonglh <phuonglh@gmail.com>
2022-06-03 12:29:46 +0700

    update

--- aka/src/main/scala/HelloWorld.scala
+++ aka/src/main/scala/HelloWorld.scala
@@ -6,7 +6,6 @@ object HelloWorld {
   final case class Greeted(whom: String, from: ActorRef[Greet])
 
   def apply(): Behavior[Greet] = Behaviors.receive { (context, message) =>
-    println("Starting the system...")
     context.log.info("Hello {}!", message.whom)
     message.replyTo ! Greeted(message.whom, context.self)
     Behaviors.same
