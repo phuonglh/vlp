350af0cdf30a51c8212fef733e3fb46a817cf9b4
phuonglh <phuonglh@gmail.com>
2022-11-30 22:07:31 +0700

    Add preprocessing for FTEL

---
+++ vdg-l/src/main/scala/vlp/vdg/Preprocessor.scala
@@ -0,0 +1,57 @@
+package vlp.vdg
+
+import scala.io.Source
+import java.nio.file.Files
+import java.io.File
+import java.io.BufferedWriter
+import java.io.FileWriter
+import java.nio.charset.StandardCharsets
+
+/**
+  * phuonglh, for FTEL address project.
+  * November 30, 2022
+  * 
+  */
+object Preprocessor {
+  val map = Map(
+    """P\.(?!\d)""" -> "Phường ", // ambiguity with P.1, P.2 (ward of HCMC)
+    "Q6" -> "Quận 6",
+    """TP\.""" -> "Thành phố ",
+    """T\.[Tt]rệt""" ->	"Tầng Trệt",
+    """H\.""" -> "Huyện ",
+    "TT" -> "Thị trấn",
+    """T\.[Tt]rấn""" ->	"Thị trấn",
+    """TX\.""" -> "Thị xã ",
+    """\(.+\)""" -> "", // remove everything inside parentheses    
+  )
+
+  def replace(input: String): String = {
+    var s = input
+    for (u <- map.keySet) {
+      s = s.replaceAll(u, map.get(u).get)
+    }
+    return s.replaceAll("""\s+""", " ").replaceAll(" ,", ",")
+  }
+
+  def main(args: Array[String]): Unit = {
+    if (args.isEmpty) {
+      System.out.println("Need an input text file!")
+      return
+    }
+    val inp = args(0)
+    val out = if (args.size > 1) args(1) else ""
+    val xs = Source.fromFile(inp, "UTF-8").getLines().toList
+    val ys = xs.par.map(x => replace(x)).toList
+    if (out.nonEmpty) {
+      val file = new File(out)
+      val bw = new BufferedWriter(new FileWriter(file, StandardCharsets.UTF_8))
+      for (y <- ys) {
+        bw.write(y)
+        bw.write("\n")
+      }
+      bw.close()
+    } else {
+      ys.foreach(println)
+    }
+  }
+}
