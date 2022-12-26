427d731ee905c0a098fdde2df2332e860c2dbb36
phuonglh <phuonglh@gmail.com>
2022-12-26 23:17:57 +0700

    Update con

--- con/src/main/java/vlp/con/TreebankReader.java
+++ con/src/main/java/vlp/con/TreebankReader.java
@@ -1,57 +1,57 @@
-package vlp.con;
+// package vlp.con;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringReader;
-import java.util.function.Consumer;
-import java.util.function.Function;
+// import java.io.FileWriter;
+// import java.io.IOException;
+// import java.io.PrintWriter;
+// import java.io.StringReader;
+// import java.util.function.Consumer;
+// import java.util.function.Function;
 
-import edu.stanford.nlp.trees.DiskTreebank;
-import edu.stanford.nlp.trees.PennTreeReader;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.trees.TreeLeafLabelTransformer;
-import edu.stanford.nlp.trees.TreeReader;
-import edu.stanford.nlp.trees.TreeTransformer;
-import edu.stanford.nlp.trees.Treebank;
+// import edu.stanford.nlp.trees.DiskTreebank;
+// import edu.stanford.nlp.trees.PennTreeReader;
+// import edu.stanford.nlp.trees.Tree;
+// import edu.stanford.nlp.trees.TreeLeafLabelTransformer;
+// import edu.stanford.nlp.trees.TreeReader;
+// import edu.stanford.nlp.trees.TreeTransformer;
+// import edu.stanford.nlp.trees.Treebank;
 
-public class TreebankReader {
+// public class TreebankReader {
 
-  static class LeafReplacement implements Function<String, String> {
-    @Override
-    public String apply(String t) {
-      System.out.println(t);
-      return t.replaceAll("\\s+", "_").toLowerCase();
-    }
-  }
+//   static class LeafReplacement implements Function<String, String> {
+//     @Override
+//     public String apply(String t) {
+//       System.out.println(t);
+//       return t.replaceAll("\\s+", "_").toLowerCase();
+//     }
+//   }
 
-  static class TreeConsumer implements Consumer<Tree> {
-    PrintWriter pw;
-    TreeTransformer tt = new TreeLeafLabelTransformer(new LeafReplacement());
+//   static class TreeConsumer implements Consumer<Tree> {
+//     PrintWriter pw;
+//     TreeTransformer tt = new TreeLeafLabelTransformer(new LeafReplacement());
 
-    public TreeConsumer(PrintWriter pw) {
-      this.pw = pw;
-    }
-    @Override
-    public void accept(Tree t) {
-      pw.write(t.transform(tt).toString());
-      pw.append("\n");
-    }    
-  }
+//     public TreeConsumer(PrintWriter pw) {
+//       this.pw = pw;
+//     }
+//     @Override
+//     public void accept(Tree t) {
+//       pw.write(t.transform(tt).toString());
+//       pw.append("\n");
+//     }    
+//   }
 
-  public static void main(String[] args) throws IOException {
-    Treebank tb = new DiskTreebank();
-    tb.loadPath("../dat/con/file_3.2.txt");
+//   public static void main(String[] args) throws IOException {
+//     Treebank tb = new DiskTreebank();
+//     tb.loadPath("../dat/con/file_3.2.txt");
 
-    PrintWriter pw = new PrintWriter(new FileWriter("dat/3.2.txt"));
+//     PrintWriter pw = new PrintWriter(new FileWriter("dat/3.2.txt"));
 
-    tb.forEach(new TreeConsumer(pw));
-    pw.close();
-    System.out.println("Number of trees = " + tb.size());
-    // StringReader st = new StringReader("(S (NP (N He he)) (VP (V goes)))");
-    // TreeReader tr = new PennTreeReader(st);
-    // Tree tree = tr.readTree();
-    // tree.pennPrint();
-    // tr.close();
-  }
-}
+//     tb.forEach(new TreeConsumer(pw));
+//     pw.close();
+//     System.out.println("Number of trees = " + tb.size());
+//     // StringReader st = new StringReader("(S (NP (N He he)) (VP (V goes)))");
+//     // TreeReader tr = new PennTreeReader(st);
+//     // Tree tree = tr.readTree();
+//     // tree.pennPrint();
+//     // tr.close();
+//   }
+// }
---
+++ con/src/main/scala/vlp/con/Parser.scala
@@ -0,0 +1,84 @@
+package vlp.con
+
+import com.intel.analytics.bigdl.dllib.NNContext
+import com.intel.analytics.bigdl.dllib.keras.{Model, Sequential}
+import com.intel.analytics.bigdl.dllib.keras.models.Models
+import com.intel.analytics.bigdl.dllib.keras.optimizers.Adam
+import com.intel.analytics.bigdl.dllib.nn.ClassNLLCriterion
+import com.intel.analytics.bigdl.dllib.utils.Shape
+import com.intel.analytics.bigdl.dllib.keras.layers._
+import com.intel.analytics.bigdl.numeric.NumericFloat
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.DoubleType
+import org.apache.spark.sql.Row
+import com.intel.analytics.bigdl.dllib.nnframes.NNImageReader
+import org.apache.hadoop.fs.Path
+import com.intel.analytics.bigdl.dllib.utils.Engine
+import org.apache.log4j.{Logger, Level}
+import com.intel.analytics.bigdl.dllib.feature.image.ImageChannelNormalize
+import com.intel.analytics.bigdl.dllib.keras.objectives.BinaryCrossEntropy
+import com.intel.analytics.bigdl.dllib.optim.Top1Accuracy
+
+
+object Parser {
+  
+ def buildMode(inputShape: Shape): Sequential[Float] = {
+    val model = Sequential()
+    model.add(Conv2D(32, 3, 3, inputShape = inputShape))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(poolSize = (2, 2)))
+
+    model.add(Conv2D(32, 3, 3))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(poolSize = (2, 2)))
+
+    model.add(Conv2D(64, 3, 3))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(poolSize = (2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Activation("relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation("sigmoid"))
+
+    return model
+  }
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
+    val conf = Engine.createSparkConf().setAppName("Parser").setMaster("local[4]")
+      .set("spark.executor.cores", "1")
+      .set("spark.cores.max", "8")
+      .set("spark.executor.memory", "4g")
+      .set("spark.driver.memory", "4g")
+    val sparkContext = new SparkContext(conf)
+    Engine.init
+
+    val sc = NNContext.initNNContext("vlp.con.Parser")
+    // read the "cats_dogs/train" folder and create labeled images: cat -> 1, dog -> 2
+    val createLabel = udf { row: Row => 
+      if (new Path(row.getString(0)).getName.contains("cat")) 1 else 2 
+    }
+    val imagePath = "dat/cats_dogs/demo/"
+    val imageDF = NNImageReader.readImages(imagePath, sc, resizeH = 150, resizeW = 150)
+    val df = imageDF.withColumn("label", createLabel(col("image")))
+    df.printSchema()
+    df.show()
+    // train/test split
+    val Array(trainingDF, validationDF) = df.randomSplit(Array(0.8, 0.2), seed = 80L)
+
+    val model = buildMode(Shape(3, 150, 150))
+    model.compile(optimizer = new Adam(), loss = BinaryCrossEntropy(), metrics = List(new Top1Accuracy()))
+
+    // preprocess the images before training
+    val transformers = ImageChannelNormalize(0, 0, 0, 255, 255, 255)
+    model.fit(trainingDF, batchSize = 64, nbEpoch = 10, labelCols = Array("label"), transform = transformers, valX = validationDF)
+
+    sc.stop()
+  }
+}
