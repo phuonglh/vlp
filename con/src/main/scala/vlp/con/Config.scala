9a415ced9bdea2d0fa5986a642b56f944df65820
phuonglh <phuonglh@gmail.com>
2023-01-25 16:19:54 +0700

    Update

--- con/src/main/scala/vlp/con/Config.scala
+++ con/src/main/scala/vlp/con/Config.scala
@@ -1,6 +1,25 @@
 package vlp.con
 
 /**
+ * @param hiddenSize size of the encoder layers (embeddingSize)
+ * @param nBlock block number
+ * @param nHead head number
+ * @param maxPositionLen sequence length, default is 512
+ * @param intermediateSize The size of the "intermediate" (i.e., feed-forward)
+ * @param hiddenPDrop The dropout probability for all fully connected layers
+ * @param attnPDrop drop probability of attention
+*/
+case class ConfigBERT(
+  hiddenSize: Int = 96,        // 8 times less than the default 768
+  nBlock: Int = 2,             // default is 12
+  nHead: Int = 2,              // default is 12
+  maxPositionLen: Int = 30,    // should be the same as config.maxSeqLen
+  intermediateSize: Int = 96,  // 32 times less than the default 3072
+  hiddenPDrop: Double = 0.1,   // default value of BigDL BERT
+  attnPDrop: Double = 0.1      // default value of BigDL BERT
+) extends Serializable 
+
+/**
   * phuonglh@gmail.com
   *
   */
@@ -31,3 +50,4 @@ case class Config(
   modelType: String = "tb", // tk: token LSTM, ch: character LSTM, tb: token BERT, cb: character BERT
   bert: ConfigBERT = ConfigBERT()
 ) extends Serializable
+
