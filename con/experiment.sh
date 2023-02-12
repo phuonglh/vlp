971738605e81356c6a5b16a427228969d754fe09
phuonglh <phuonglh@gmail.com>
2023-02-12 17:23:19 +0700

    Update

--- con/experiment.sh
+++ con/experiment.sh
@@ -2,14 +2,18 @@
 
 # phuonglh for batch experiments with multiple languages and models
 
-languages = "czech english german italian swedish"
+languages="czech english german italian swedish"
+echo $languages
 
 for language in $languages
+do
+  echo $language
   # tk and st
   for modelType in "tk st"
-    bloop -p root -m vlp.con.VSC -- -g -l $language -m experiment-tk-st -t $modelType
+  do	  
+    bloop run -p root -m vlp.con.VSC -- -g -l $language -m experiment-tk-st -t $modelType
   done
   # ch
-  bloop -p root -m vlp.con.VSC -- -g -l $language -m experiment-ch -t ch
+  bloop run -p root -m vlp.con.VSC -- -g -l $language -m experiment-ch -t ch
 done
 
