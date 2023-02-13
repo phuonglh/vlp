7d570998c890b854007a2471f59660784a1b11c5
phuonglh <phuonglh@gmail.com>
2023-02-13 21:51:20 +0700

    Update woz

--- woz/src/main/scala/vlp/woz/DomainFilter.scala
+++ woz/src/main/scala/vlp/woz/DomainFilter.scala
@@ -1,5 +1,6 @@
 package vlp.woz
 
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.functions._
@@ -10,12 +11,11 @@ import org.apache.spark.sql.catalyst.ScalaReflection
 object DomainFilter {
 
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName(getClass.getName())
-      .config("master", "local[4]")
-      .getOrCreate()
+    val conf = new SparkConf().setAppName(getClass().getName()).setMaster("local[2]")
+    val spark = SparkSession.builder.config(conf).getOrCreate()
     import spark.implicits._
 
-    val path = "woz/dat/woz/data/MultiWOZ_2.2/dev/dialogues_001.json"
+    val path = "dat/woz/data/MultiWOZ_2.2/dev/dialogues_001.json"
     val domain = "train"
     // SlotValues are temporarily set to empty. TODO: give a complete description for this struct type. 
     // We use a ScalaReflection hack to overcome a schema error.
@@ -35,6 +35,7 @@ object DomainFilter {
 
     kf.show()
     println(kf.count())
+    
     spark.close()
   }
 }
