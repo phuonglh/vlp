7b736bd7dd94e29f2756ad60848c1b308d21b6bd
phuonglh <phuonglh@gmail.com>
2023-02-17 17:20:52 +0700

    Update

--- woz/src/main/scala/vlp/woz/act/DialogActFilter.scala
+++ woz/src/main/scala/vlp/woz/act/DialogActFilter.scala
@@ -1,11 +1,9 @@
 package vlp.woz.act
 
-import scala.util.parsing.json._
 import org.json4s._
-import org.json4s.jackson.Serialization
+import org.json4s.jackson.JsonMethods._
 import java.nio.file.{Paths, Files, StandardOpenOption}
 import java.nio.charset.StandardCharsets
-import scala.collection.mutable.ListBuffer
 
 
 case class Act(
@@ -26,27 +24,30 @@ case class Dialog(
 object DialogActFilter {
   def main(args: Array[String]): Unit = {
     implicit val formats = DefaultFormats
-    val path = "/Users/phuonglh/vlp/woz/dat/woz/003.json"
+    val path = "dat/woz/003.json"
     val content = scala.io.Source.fromFile(path).getLines().toList.mkString(" ")
-    // unmarshal to a map of dialogs 
-    val dialogs = JSON.parseFull(content).get.asInstanceOf[Map[String,Any]]
-    // extract dialog ids and turns
-    val ds = dialogs.keySet.map { id =>
-      val turnMap = dialogs(id).asInstanceOf[Map[String, Any]]
-      // get sorted turnIds
-      val turnIds = turnMap.keySet.toList.sorted
-      // get corresponding acts
-      val turns = turnIds.map { id =>
-        val elements = turnMap(id).asInstanceOf[Map[String, Any]]
-        val dialogAct = elements.get("dialog_act").asInstanceOf[Map[String, List[List[String]]]]
-        val acts = dialogAct.keySet.map { name => 
-          val frames = dialogAct(name).map(list => (list(0), list(1)))
-          Act(name, frames)
-        }
-        Turn(id, acts)
-      }
-      Dialog(id, turns)
-    }
-    ds.foreach(println)
+    val ds = parse(content)
+    pretty(render(ds))
+    
+    // // unmarshal to a map of dialogs 
+    // val dialogs = JSON.parseFull(content).get.asInstanceOf[Map[String,Any]]
+    // // extract dialog ids and turns
+    // val ds = dialogs.keySet.map { id =>
+    //   val turnMap = dialogs(id).asInstanceOf[Map[String, Any]]
+    //   // get sorted turnIds
+    //   val turnIds = turnMap.keySet.toList.sorted
+    //   // get corresponding acts
+    //   val turns = turnIds.map { id =>
+    //     val elements = turnMap(id).asInstanceOf[Map[String, Any]]
+    //     val dialogAct = elements.get("dialog_act").asInstanceOf[Map[String, List[List[String]]]]
+    //     val acts = dialogAct.keySet.map { name => 
+    //       val frames = dialogAct(name).map(list => (list(0), list(1)))
+    //       Act(name, frames)
+    //     }
+    //     Turn(id, acts)
+    //   }
+    //   Dialog(id, turns)
+    // }
+    // ds.foreach(println)
   }
 }
