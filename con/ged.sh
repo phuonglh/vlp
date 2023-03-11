70563bcc8a7bda37a1df6ec41cca8aad7a5c56b0
phuonglh <phuonglh@gmail.com>
2023-03-11 19:14:59 +0700

    Update

--- con/ged.sh
+++ con/ged.sh
@@ -6,5 +6,5 @@ outputs=("cs_tk.tsv" "en_fce_tk.tsv" "de_tk.tsv" "it_tk.tsv" "sv_tk.tsv")
 
 for i in "${!languages[@]}"; do
   # echo $i, ${languages[$i]}, "dat/out/${outputs[$i]}"
-  bloop run -p root -m vlp.con.VSC -- -g -l ${languages[$i]} -t tk -m predict -o dat/out/${outputs[$i]}
+  bloop run -p root -m vlp.con.VSC -- -g -l ${languages[$i]} -t tk -m predict -o dat/out/${outputs[$i]} -J-Xmx8g
 done
