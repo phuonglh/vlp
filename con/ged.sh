d885ddaa45ac2159c7c17e3096eb72881bac7d9b
phuonglh <phuonglh@gmail.com>
2023-03-12 09:26:35 +0700

    Update

--- con/ged.sh
+++ con/ged.sh
@@ -2,9 +2,9 @@
 
 # phuonglh for batch experiments with multiple languages and models
 languages=("czech" "english" "german" "italian" "swedish")
-outputs=("cs_tk.tsv" "en_fce_tk.tsv" "de_tk.tsv" "it_tk.tsv" "sv_tk.tsv")
+outputs=("cs" "en_fce" "de" "it" "sv")
 
 for i in "${!languages[@]}"; do
   # echo $i, ${languages[$i]}, "dat/out/${outputs[$i]}"
-  bloop run -p root -m vlp.con.VSC -- -g -l ${languages[$i]} -t tk -m predict -o dat/out/${outputs[$i]} -J-Xmx8g
+  bloop run -p root -m vlp.con.VSC -- -g -l ${languages[$i]} -t ch -m predict -o "dat/out/${outputs[$i]}_ch.tsv" -J-Xmx8g
 done
