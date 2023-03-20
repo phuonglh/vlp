db3c3963e473e384e81ea4e41134a800aa7b97ce
phuonglh <phuonglh@gmail.com>
2023-03-20 21:44:25 +0700

    Update

--- woz/src/main/scala/vlp/woz/jsl/ConfigJSL.scala
+++ woz/src/main/scala/vlp/woz/jsl/ConfigJSL.scala
@@ -5,11 +5,12 @@ case class ConfigJSL(
   totalCores: Int = 8,    // X
   executorCores: Int = 8, // Y ==> there are Y/X executors 
   executorMemory: String = "8g", // Z
-  driverMemory: String = "8g", // D
+  driverMemory: String = "16g", // D
+  language: String = "en", // {en, vi}
   mode: String = "eval",
   batchSize: Int = 128,
   learningRate: Double = 5E-4,
-  epochs: Int = 80,
+  epochs: Int = 200,
   modelPath: String = "bin/act/",
   trainPath: String = "dat/woz/act/train/",
   devPath: String = "dat/woz/act/dev/",
