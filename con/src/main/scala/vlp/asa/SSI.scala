dfa77f6ce743c42ba4b6adca7af6e6edd7a7ed30
phuonglh <phuonglh@gmail.com>
2023-04-17 01:10:39 +0700

    Update

--- con/src/main/scala/vlp/asa/SSI.scala
+++ con/src/main/scala/vlp/asa/SSI.scala
@@ -2,9 +2,11 @@ package vlp.asa
 
 import java.nio.file.{Files, Paths, StandardOpenOption}
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.ml.feature.{Tokenizer, CountVectorizer, StringIndexer}
 import org.apache.spark.ml.{Pipeline, PipelineModel}
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.functions._
 
@@ -33,17 +35,57 @@ case class Sample(
 case class Instance(postText: String, postSentiments: Array[String], commentText: String, commentSentiments: Array[String])
 
 object SSI {
-  def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder.getOrCreate()
+
+  /**
+    * Reads in the data set and performs preprocessing as follows: (1) Each post or comment is assigned 
+    * the sentiment which is the most frequent (say, if a comment which has 3 postive labels and 2 negative labels then it 
+    * is consider positive); if there is not any sentiments, the NA label is returned. (2) Each post is flat-mapped with its comments.
+    *
+    * @param spark
+    * @param path
+    */
+  def readData(spark: SparkSession, path: String): DataFrame = {
     import spark.implicits._
-    val path = "dat/ssi/project-28-at-2023-02-21-10-28-518a1b2d.json-formatted-4.json"
     val df = spark.read.option("multiline", "true").json(path).as[Sample]
+    // find the most frequent sentiment of a document, or NA if the document does not have any sentiment.
+    def f(ass: Array[AspectSentiment]) = {
+      val sentiments = ass.map(e => e.sentiment).groupBy(identity).toList.map(p => (p._1, p._2.size)).sortBy(-_._2)
+      if (sentiments.isEmpty) "NA" else sentiments.head._1
+    }
     // flat map the dataset based on comments
-    def f(ass: Array[AspectSentiment]) = ass.map(e => e.aspect + "-" + e.sentiment)
-    val af = df.flatMap(sample => sample.comments.map(c => (sample.text, f(sample.aspect_sentiments), c.text, f(c.aspect_sentiments))))
-      .toDF("postText", "postSentiments", "commentText", "commentSentiments")
-    // remove samples having empty sentiment annotation (both in comment and post)
-    val bf = af.filter(size(col("postSentiments")) > 0 || size(col("commentSentiments")) > 0)
+    df.flatMap(sample => sample.comments.map(c => (sample.text, f(sample.aspect_sentiments), c.text, f(c.aspect_sentiments)))).toDF("postText", "postSentiment", "commentText", "commentSentiment")
+  }
+
+  def filterComments(df: DataFrame): DataFrame = {
+    df.filter(col("commentSentiment") =!= "NA").filter(col("commentSentiment") =!= "unrelated")
+  }
+
+  def fit(df: DataFrame) = {
+    val labelIndexer = new StringIndexer().setInputCol("commentSentiment").setOutputCol("label")
+    val tokenizer = new Tokenizer().setInputCol("text").setOutputCol("tokens")
+    val vectorizer = new CountVectorizer().setInputCol("tokens").setOutputCol("features").setVocabSize(512)
+    val classifier = new MultilayerPerceptronClassifier().setLayers(Array(512, 64, 3)).setBlockSize(32).setSeed(1234L).setMaxIter(50)
+    val pipeline = new Pipeline().setStages(Array(labelIndexer, tokenizer, vectorizer, classifier))
+    val model = pipeline.fit(df)
+    val result = model.transform(df)
+    result.show
+    val predictionAndLabels = result.select("prediction", "label")
+    val evaluator = new MulticlassClassificationEvaluator().setMetricName("f1")
+    println(s"f1 = ${evaluator.evaluate(predictionAndLabels)}")
+  }
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName(getClass().getName()).setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("INFO")
+    val spark = SparkSession.builder.config(sc.getConf).getOrCreate()
+    val path = "dat/ssi/project-28-at-2023-02-21-10-28-518a1b2d.json-formatted-4.json"
+    val af = readData(spark, path)
+    val bf = filterComments(af)
+    bf.show
+    // concatenate two text columns
+    val df = bf.withColumn("text", concat(col("postText"), col("commentText")))
+    fit(df)
     spark.stop()
   }
 }
