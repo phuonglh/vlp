2114366f82f61558c07a43a42c36e93ac098a65f
phuonglh <phuonglh@gmail.com>
2023-06-11 15:04:40 +0700

    Update

--- vcb/src/main/scala/Stock.scala
+++ vcb/src/main/scala/Stock.scala
@@ -1,18 +1,36 @@
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.feature.{StandardScaler, VectorAssembler}
+import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.sql.expressions.Window
-import org.apache.spark.sql.functions.{col, lead, to_date}
+import org.apache.spark.sql.functions.{col, isnull, lead, not, to_date}
+import com.intel.analytics.bigdl.numeric.NumericFloat
+import com.intel.analytics.bigdl.dllib.keras.{Model, Sequential}
+import com.intel.analytics.bigdl.dllib.keras.models.KerasNet
+import com.intel.analytics.bigdl.dllib.keras.layers._
+import com.intel.analytics.bigdl.dllib.nn.MSECriterion
+import com.intel.analytics.bigdl.dllib.tensor.Tensor
+import com.intel.analytics.bigdl.dllib.utils.{Engine, Shape}
+import com.intel.analytics.bigdl.dllib.nn.abstractnn.{AbstractModule, Activity}
+import com.intel.analytics.bigdl.dllib.nn.internal.KerasLayer
+import com.intel.analytics.bigdl.dllib.nnframes.NNModel
+import com.intel.analytics.bigdl.dllib.visualization.{TrainSummary, ValidationSummary}
+import com.intel.analytics.bigdl.dllib.nnframes.{NNEstimator, NNModel}
+import com.intel.analytics.bigdl.dllib.optim.{Adam, Loss, MAE, Trigger}
+import org.apache.spark.SparkContext
 
 case class ConfigStock(
-  val lead: Int = 5
+  lead: Int = 5,
+  recurrentSize: Int = 8,
+  batchSize: Int = 16,
+  learningRate: Float = 1E-4f,
+  epochs: Int = 5,
 )
 
 object Stock {
 
-  private def preprocess(af: DataFrame, config: ConfigStock): DataFrame = {
-    val assembler = new VectorAssembler().setInputCols(Array("Open", "High", "Low", "Close", "Volume")).setOutputCol("xRaw")
+  def preprocess(af: DataFrame, config: ConfigStock): DataFrame = {
+    val assembler = new VectorAssembler().setInputCols(Array("Open", "High", "Low", "Close", "Volume")).setOutputCol("xRaw").setHandleInvalid("skip")
     val scalerX = new StandardScaler().setInputCol("xRaw").setOutputCol("x0")
     val pipeline = new Pipeline().setStages(Array(assembler, scalerX))
     val pipelineModel = pipeline.fit(af) // TODO: save params for later re-scaling
@@ -28,23 +46,67 @@ object Stock {
     }
     df.show(false)
     val colNames = (0 until config.lead).map(k => s"x$k").toArray
-    val concat = new VectorAssembler().setInputCols(colNames).setOutputCol("x")
+    val concat = new VectorAssembler().setInputCols(colNames).setOutputCol("x").setHandleInvalid("skip")
     concat.transform(ef).select("x", "yRaw")
+      .filter(not(isnull(col("yRaw"))))
+      .withColumn("y", col("yRaw")/1000)
+  }
+
+  def createModel(config: ConfigStock): KerasNet[Float] = {
+    val model = Sequential()
+    model.add(Reshape(Array(config.lead, 5), inputShape = Shape(5*config.lead)))
+    model.add(LSTM(outputDim = config.recurrentSize, returnSequences = false))
+    model.add(Dense(outputDim = 1))
+    model
   }
 
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("Stock").master("local").getOrCreate()
+//    val spark = SparkSession.builder().appName("Stock").master("local").getOrCreate()
+    val conf = Engine.createSparkConf().setAppName(getClass().getName()).setMaster("local[*]")
+//      .set("spark.executor.cores", config.executorCores.toString)
+//      .set("spark.cores.max", config.totalCores.toString)
+//      .set("spark.executor.memory", config.executorMemory)
+//      .set("spark.driver.memory", config.driverMemory)
+    val sc = new SparkContext(conf)
+    Engine.init
+
+    val spark = SparkSession.builder.config(sc.getConf).getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
 
-    val af = spark.read.options(Map("header" -> "true", "inferSchema" -> "true"))
-      .csv("dat/HOSE_2008_2022_vnstock.csv")
-      .filter(col("Stock") === "VNM")
+    val af = spark.read.options(Map("header" -> "true", "inferSchema" -> "true")).csv("dat/HOSE_2008_2022_vnstock.csv").filter(col("Stock") === "VNM")
     af.show(false)
     println(af.count())
-
-    val df = preprocess(af, ConfigStock())
+    val config = ConfigStock()
+    val df = preprocess(af, config)
     df.show(false)
     df.printSchema()
+    println(df.count())
+
+//    val lr = new LinearRegression().setFeaturesCol("x").setLabelCol("y")
+//    val lrModel = lr.fit(df)
+//    // Print the coefficients and intercept for linear regression
+//    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+//
+//    // Summarize the model over the training set and print out some metrics
+//    val trainingSummary = lrModel.summary
+//    println(s"numIterations: ${trainingSummary.totalIterations}")
+//    println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
+//    trainingSummary.residuals.show()
+//    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+//    println(s"r2: ${trainingSummary.r2}")
+
+    val bigdl = createModel(config)
+    val trainingSummary = TrainSummary(appName = "stock", logDir = "sum")
+    val validationSummary = ValidationSummary(appName = "stock", logDir = "sum")
+    val estimator = NNEstimator(bigdl, MSECriterion(), Array(25), Array(1))
+    estimator.setLabelCol("y").setFeaturesCol("x")
+      .setBatchSize(config.batchSize)
+      .setOptimMethod(new Adam(config.learningRate))
+      .setMaxEpoch(config.epochs)
+      .setTrainSummary(trainingSummary)
+      .setValidationSummary(validationSummary)
+      .setValidation(Trigger.everyEpoch, df, Array(new MAE()), config.batchSize)
+    estimator.fit(df)
 
     spark.stop
   }
