6a616bcc30fa820585ee92dd39f455f9cf2c3bad
phuonglh <phuonglh@gmail.com>
2023-06-11 19:02:31 +0700

    Update

--- vcb/build.sbt
+++ vcb/build.sbt
@@ -2,7 +2,8 @@ ThisBuild / version := "1.0"
 
 ThisBuild / scalaVersion := "2.12.12"
 
-val sparkVersion = "3.3.2"
+val sparkVersion = "3.1.2"
+val bigdlVersion = "2.1.0"
 
 lazy val root = (project in file("."))
   .settings(
@@ -10,6 +11,8 @@ lazy val root = (project in file("."))
     libraryDependencies ++= Seq(
       "org.apache.spark" %% "spark-core" % sparkVersion,
       "org.apache.spark" %% "spark-sql" % sparkVersion,
-      "org.apache.spark" %% "spark-mllib" % sparkVersion
+      "org.apache.spark" %% "spark-mllib" % sparkVersion,
+      "com.intel.analytics.bigdl" % "bigdl-dllib-spark_3.1.2" % bigdlVersion,
+      "com.google.protobuf" % "protobuf-java" % "3.21.9",
     )
   )
--- vcb/src/main/scala/Stock.scala
+++ vcb/src/main/scala/Stock.scala
@@ -5,31 +5,33 @@ import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.{col, isnull, lead, not, to_date}
 import com.intel.analytics.bigdl.numeric.NumericFloat
-import com.intel.analytics.bigdl.dllib.keras.{Model, Sequential}
+import com.intel.analytics.bigdl.dllib.keras.Sequential
 import com.intel.analytics.bigdl.dllib.keras.models.KerasNet
 import com.intel.analytics.bigdl.dllib.keras.layers._
 import com.intel.analytics.bigdl.dllib.nn.MSECriterion
-import com.intel.analytics.bigdl.dllib.tensor.Tensor
 import com.intel.analytics.bigdl.dllib.utils.{Engine, Shape}
-import com.intel.analytics.bigdl.dllib.nn.abstractnn.{AbstractModule, Activity}
-import com.intel.analytics.bigdl.dllib.nn.internal.KerasLayer
-import com.intel.analytics.bigdl.dllib.nnframes.NNModel
 import com.intel.analytics.bigdl.dllib.visualization.{TrainSummary, ValidationSummary}
 import com.intel.analytics.bigdl.dllib.nnframes.{NNEstimator, NNModel}
-import com.intel.analytics.bigdl.dllib.optim.{Adam, Loss, MAE, Trigger}
+import com.intel.analytics.bigdl.dllib.optim.{Adam, Trigger}
+import com.intel.analytics.bigdl.dllib.keras.metrics.{MSE, MAE}
+
 import org.apache.spark.SparkContext
 
 case class ConfigStock(
-  lead: Int = 5,
-  recurrentSize: Int = 8,
-  batchSize: Int = 16,
-  learningRate: Float = 1E-4f,
-  epochs: Int = 5,
+  lead: Int = 10,
+  recurrentSize: Int = 64,
+  batchSize: Int = 32,
+  learningRate: Float = 1E-3f,
+  epochs: Int = 40,
+  executorCores: String = "4",
+  totalCores: String = "8",
+  executorMemory: String = "4g",
+  driverMemory: String = "4g"
 )
 
 object Stock {
 
-  def preprocess(af: DataFrame, config: ConfigStock): DataFrame = {
+  private def preprocess(af: DataFrame, config: ConfigStock): DataFrame = {
     val assembler = new VectorAssembler().setInputCols(Array("Open", "High", "Low", "Close", "Volume")).setOutputCol("xRaw").setHandleInvalid("skip")
     val scalerX = new StandardScaler().setInputCol("xRaw").setOutputCol("x0")
     val pipeline = new Pipeline().setStages(Array(assembler, scalerX))
@@ -60,52 +62,55 @@ object Stock {
     model
   }
 
+  def linearRegression(df: DataFrame) = {
+    val lr = new LinearRegression().setFeaturesCol("x").setLabelCol("y")
+    val lrModel = lr.fit(df)
+    // Print the coefficients and intercept for linear regression
+    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+
+    // Summarize the model over the training set and print out some metrics
+    val trainingSummary = lrModel.summary
+    println(s"numIterations: ${trainingSummary.totalIterations}")
+    println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
+    trainingSummary.residuals.show()
+    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+    println(s"r2: ${trainingSummary.r2}")
+  }
+
   def main(args: Array[String]): Unit = {
-//    val spark = SparkSession.builder().appName("Stock").master("local").getOrCreate()
+    val config = ConfigStock()
     val conf = Engine.createSparkConf().setAppName(getClass().getName()).setMaster("local[*]")
-//      .set("spark.executor.cores", config.executorCores.toString)
-//      .set("spark.cores.max", config.totalCores.toString)
-//      .set("spark.executor.memory", config.executorMemory)
-//      .set("spark.driver.memory", config.driverMemory)
+      .set("spark.executor.cores", config.executorCores)
+      .set("spark.cores.max", config.totalCores)
+      .set("spark.executor.memory", config.executorMemory)
+      .set("spark.driver.memory", config.driverMemory)
     val sc = new SparkContext(conf)
     Engine.init
 
     val spark = SparkSession.builder.config(sc.getConf).getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
 
-    val af = spark.read.options(Map("header" -> "true", "inferSchema" -> "true")).csv("dat/HOSE_2008_2022_vnstock.csv").filter(col("Stock") === "VNM")
+    val af = spark.read.options(Map("header" -> "true", "inferSchema" -> "true"))
+      .csv("dat/HOSE_2008_2022_vnstock.csv")
+      .filter(col("Stock") === "VNM")
     af.show(false)
     println(af.count())
-    val config = ConfigStock()
     val df = preprocess(af, config)
     df.show(false)
     df.printSchema()
     println(df.count())
 
-//    val lr = new LinearRegression().setFeaturesCol("x").setLabelCol("y")
-//    val lrModel = lr.fit(df)
-//    // Print the coefficients and intercept for linear regression
-//    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
-//
-//    // Summarize the model over the training set and print out some metrics
-//    val trainingSummary = lrModel.summary
-//    println(s"numIterations: ${trainingSummary.totalIterations}")
-//    println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
-//    trainingSummary.residuals.show()
-//    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
-//    println(s"r2: ${trainingSummary.r2}")
-
     val bigdl = createModel(config)
     val trainingSummary = TrainSummary(appName = "stock", logDir = "sum")
     val validationSummary = ValidationSummary(appName = "stock", logDir = "sum")
-    val estimator = NNEstimator(bigdl, MSECriterion(), Array(25), Array(1))
+    val estimator = NNEstimator(bigdl, MSECriterion(), Array(5*config.lead), Array(1))
     estimator.setLabelCol("y").setFeaturesCol("x")
       .setBatchSize(config.batchSize)
       .setOptimMethod(new Adam(config.learningRate))
       .setMaxEpoch(config.epochs)
       .setTrainSummary(trainingSummary)
       .setValidationSummary(validationSummary)
-      .setValidation(Trigger.everyEpoch, df, Array(new MAE()), config.batchSize)
+      .setValidation(Trigger.everyEpoch, df, Array(new MSE(), new MAE()), config.batchSize)
     estimator.fit(df)
 
     spark.stop
