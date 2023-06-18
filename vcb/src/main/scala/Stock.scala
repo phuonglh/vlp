b457885a83ec481295faee5649a6cae45ca06990
phuonglh <phuonglh@gmail.com>
2023-06-19 00:35:09 +0700

    Update

--- vcb/src/main/scala/Stock.scala
+++ vcb/src/main/scala/Stock.scala
@@ -3,22 +3,19 @@ package vlp.vcb
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.feature.{StandardScaler, VectorAssembler}
-import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.{col, isnull, lead, not, to_date}
 import com.intel.analytics.bigdl.numeric.NumericFloat
 import com.intel.analytics.bigdl.dllib.keras.Sequential
-import com.intel.analytics.bigdl.dllib.keras.models.KerasNet
+import com.intel.analytics.bigdl.dllib.keras.models.{KerasNet, Models}
 import com.intel.analytics.bigdl.dllib.keras.layers._
 import com.intel.analytics.bigdl.dllib.nn.MSECriterion
 import com.intel.analytics.bigdl.dllib.utils.{Engine, Shape}
 import com.intel.analytics.bigdl.dllib.visualization.{TrainSummary, ValidationSummary}
 import com.intel.analytics.bigdl.dllib.nnframes.{NNEstimator, NNModel}
 import com.intel.analytics.bigdl.dllib.optim.{Adam, Trigger}
-import com.intel.analytics.bigdl.dllib.keras.metrics.{MSE, MAE}
-
+import com.intel.analytics.bigdl.dllib.keras.metrics.{MAE, MSE}
 import org.apache.spark.SparkContext
-
 import org.json4s._
 import org.json4s.jackson.Serialization
 import scopt.OptionParser
@@ -35,8 +32,9 @@ case class ConfigStock(
   executorMemory: String = "4g",
   driverMemory: String = "4g",
   dataPath: String = "dat/HOSE_2008_2022_vnstock.csv",
+  modelPath: String = "bin",
   stock: String = "VNM",
-  mode: String = "train"
+  mode: String = "eval"
 )
 
 /**
@@ -76,21 +74,6 @@ object Stock {
     model
   }
 
-  def linearRegression(df: DataFrame) = {
-    val lr = new LinearRegression().setFeaturesCol("x").setLabelCol("y")
-    val lrModel = lr.fit(df)
-    // Print the coefficients and intercept for linear regression
-    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
-
-    // Summarize the model over the training set and print out some metrics
-    val trainingSummary = lrModel.summary
-    println(s"numIterations: ${trainingSummary.totalIterations}")
-    println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
-    trainingSummary.residuals.show()
-    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
-    println(s"r2: ${trainingSummary.r2}")
-  }
-
   def main(args: Array[String]): Unit = {
     val opts = new OptionParser[ConfigStock](getClass.getName) {
       head(getClass.getName, "1.0")
@@ -100,6 +83,7 @@ object Stock {
       opt[String]('Z', "executorMemory").action((x, conf) => conf.copy(executorMemory = x)).text("executor memory, default is 8g")
       opt[String]('D', "driverMemory").action((x, conf) => conf.copy(driverMemory = x)).text("driver memory, default is 16g")
       opt[String]('m', "mode").action((x, conf) => conf.copy(mode = x)).text("running mode, either {eval, train, predict}")
+      opt[String]('s', "stock").action((x, conf) => conf.copy(stock = x)).text("stock symbol")
       opt[Int]('b', "batchSize").action((x, conf) => conf.copy(batchSize = x)).text("batch size")
       opt[Int]('h', "hiddenSize").action((x, conf) => conf.copy(hiddenSize = x)).text("encoder hidden size")
       opt[Int]('k', "epochs").action((x, conf) => conf.copy(epochs = x)).text("number of epochs")
@@ -111,30 +95,27 @@ object Stock {
         implicit val formats: AnyRef with Formats = Serialization.formats(NoTypeHints)
         println(Serialization.writePretty(config))
         val conf = Engine.createSparkConf().setAppName(getClass().getName()).setMaster("local[*]")
-          .set("spark.executor.cores", config.executorCores)
-          .set("spark.cores.max", config.totalCores)
-          .set("spark.executor.memory", config.executorMemory)
-          .set("spark.driver.memory", config.driverMemory)
+          .set("spark.executor.cores", config.executorCores).set("spark.cores.max", config.totalCores)
+          .set("spark.executor.memory", config.executorMemory).set("spark.driver.memory", config.driverMemory)
         val sc = new SparkContext(conf)
         Engine.init
-
         val spark = SparkSession.builder.config(sc.getConf).getOrCreate()
         spark.sparkContext.setLogLevel("ERROR")
 
-        // read the data and filter the stock symbol in interest
+        // read the data and filter the stock symbol of interest
         val af = spark.read.options(Map("header" -> "true", "inferSchema" -> "true")).csv(config.dataPath).filter(col("Stock") === config.stock)
         af.show(false)
         println(af.count())
         val df = preprocess(af, config)
         df.show(false)
         df.printSchema()
-        println(df.count())
-
+        println(s"Number of samples = ${df.count()}")
+        val Array(trainingDF, validationDF) = df.randomSplit(Array(0.8, 0.2), 1234)
         config.mode match {
           case "train" =>
             val bigdl = createModel(config)
-            val trainingSummary = TrainSummary(appName = "stock", logDir = "sum")
-            val validationSummary = ValidationSummary(appName = "stock", logDir = "sum")
+            val trainingSummary = TrainSummary(appName = config.stock, logDir = "sum")
+            val validationSummary = ValidationSummary(appName = config.stock, logDir = "sum")
             val estimator = NNEstimator(bigdl, MSECriterion(), Array(5 * config.lead), Array(1))
             estimator.setLabelCol("y").setFeaturesCol("x")
               .setBatchSize(config.batchSize)
@@ -142,9 +123,14 @@ object Stock {
               .setMaxEpoch(config.epochs)
               .setTrainSummary(trainingSummary)
               .setValidationSummary(validationSummary)
-              .setValidation(Trigger.everyEpoch, df, Array(new MSE(), new MAE()), config.batchSize)
-            estimator.fit(df)
+              .setValidation(Trigger.everyEpoch, validationDF, Array(new MSE(), new MAE()), config.batchSize)
+            estimator.fit(trainingDF)
+            // save the bigdl model
+            bigdl.saveModel(s"${config.modelPath}/${config.stock}.bigdl", overWrite = true)
+            println(bigdl.summary())
           case "eval" =>
+            val bigdl = Models.loadModel[Float](s"${config.modelPath}/${config.stock}.bigdl")
+            println(bigdl.summary())
           case _ =>
         }
         spark.stop()
