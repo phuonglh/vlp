f9d551418a8b4063c7d6900078ec690c25c6dd73
phuonglh <phuonglh@gmail.com>
2023-07-07 14:55:46 +0700

    Update

--- llm/src/main/scala/vlp/llm/OSCAR.scala
+++ llm/src/main/scala/vlp/llm/OSCAR.scala
@@ -7,21 +7,33 @@ import org.apache.spark.sql.functions._
 /**
  * OSCAR-2301 document extractor.
  * <p/>
+ * This utility reads OSCAR-23 json file(s) into a data frame, tokenizes the content field, filters all documents
+ * containing less than 80 syllables, flat map the contents into lines, filters line of length more than 40 characters
+ * and less than 1024 characters; and writes the result into a text directory (20 partitions).
+ *
+ *
+ * <p/>
  * phuonglh, May 24, 2023
  *
  */
 object OSCAR {
   def main(args: Array[String]): Unit = {
-    val pathInp = if (args.size > 0) args(0) else "/mnt/nfs/dataset/OSCAR/23"
-    val pathOut = if (args.size > 1) args(1) else "/mnt/nfs/dataset/OSCAR/23-raw"
-    val spark = SparkSession.builder().master("local").appName("OSCAR").getOrCreate()
+    val pathInp = if (args.size > 0) args(0) else "dat/23"
+    val pathOut = if (args.size > 1) args(1) else "pre/23"
+    val spark = SparkSession.builder().master("local").config("spark.driver.memory", "8g").appName("OSCAR").getOrCreate()
     val df = spark.read.option("inferSchema", "true").json(pathInp).select("content")
     // filter all documents having more than 80 tokens
     val tokenizer = new Tokenizer().setInputCol("content").setOutputCol("tokens")
     val ef = tokenizer.transform(df)
     val ff = ef.withColumn("size", size(col("tokens"))).filter(col("size") >= 80)
-    print(s"Number of documents = ${ff.count}")
-    ff.select("content").repartition(20).write.mode(SaveMode.Overwrite).json(pathOut)
+    print(s"Number of filtered documents = ${ff.count}\n")
+    // split the documents by the new-line character
+    import spark.implicits._
+    val gf = ff.select("content").flatMap(row => row.getAs[String]("content").split("""\n+""")).toDF("line")
+    // filter lines based on their length
+    val hf = gf.withColumn("length", length(col("line"))).filter(col("length") >= 40 && col("length") <= 1024)
+    print(s"Number of filtered lines = ${hf.count}\n")
+    hf.select("line").repartition(20).write.mode(SaveMode.Overwrite).text(pathOut)
     spark.stop()
   }
 }
