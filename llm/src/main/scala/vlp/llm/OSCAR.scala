39a89d26dd96a064b3bb133b0a3653c07d7696c0
phuonglh <phuonglh@gmail.com>
2023-07-11 14:26:15 +0700

    Update

--- llm/src/main/scala/vlp/llm/OSCAR.scala
+++ llm/src/main/scala/vlp/llm/OSCAR.scala
@@ -20,7 +20,7 @@ object OSCAR {
   def main(args: Array[String]): Unit = {
     val pathInp = if (args.size > 0) args(0) else "dat/23"
     val pathOut = if (args.size > 1) args(1) else "pre/23"
-    val spark = SparkSession.builder().master("local").config("spark.driver.memory", "8g").appName("OSCAR").getOrCreate()
+    val spark = SparkSession.builder().master("local").config("spark.driver.memory", "12g").appName("OSCAR").getOrCreate()
     val df = spark.read.option("inferSchema", "true").json(pathInp).select("content")
     // filter all documents having more than 80 tokens
     val tokenizer = new Tokenizer().setInputCol("content").setOutputCol("tokens")
@@ -33,7 +33,7 @@ object OSCAR {
     // filter lines based on their length
     val hf = gf.withColumn("length", length(col("line"))).filter(col("length") >= 40 && col("length") <= 2048)
     print(s"Number of filtered lines = ${hf.count}\n")
-    hf.select("line").repartition(20).write.mode(SaveMode.Overwrite).text(pathOut)
+    hf.select("line").distinct().repartition(10).write.mode(SaveMode.Overwrite).text(pathOut)
     spark.stop()
   }
 }
