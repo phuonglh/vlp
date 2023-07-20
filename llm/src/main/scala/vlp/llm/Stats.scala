fdaf9b653f87b96c6a3f6b024b3d611ea0020ea4
phuonglh <phuonglh@gmail.com>
2023-07-20 12:41:59 +0700

    Update

---
+++ llm/src/main/scala/vlp/llm/Stats.scala
@@ -0,0 +1,24 @@
+package vlp.llm
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+/**
+ * Compute some statistics of the OSCAR corpus.
+ */
+object Stats {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().master("local[*]").config("spark.driver.memory", "16g").appName("OSCAR").getOrCreate()
+    spark.sparkContext.setLogLevel("INFO")
+    val pathInp = args(0)
+    val cf = spark.read.option("inferSchema", true).option("recursiveFileLookup", true).json(pathInp)
+    println("Number of documents = " + cf.count())
+    import spark.implicits._
+    val df = cf.map { row =>
+      row.getAs[Seq[String]](0).flatMap(line => line.split("\\s+")).size
+    }.toDF("size")
+    val ef = df.agg(sum(col("size")))
+    ef.show()
+    spark.stop()
+  }
+}
