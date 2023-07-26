247ae593e1db6a0ccc076b175d4d1d7317197bfc
phuonglh <phuonglh@gmail.com>
2023-07-26 10:26:54 +0700

    Update

--- llm/src/main/scala/vlp/llm/Stats.scala
+++ llm/src/main/scala/vlp/llm/Stats.scala
@@ -8,15 +8,14 @@ import org.apache.spark.sql.functions._
  */
 object Stats {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().master("local[*]").config("spark.driver.memory", "16g").appName("OSCAR").getOrCreate()
-    spark.sparkContext.setLogLevel("INFO")
+    val spark = SparkSession.builder().master("local[*]")
+      .config("spark.driver.memory", "16g").appName("OSCAR").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
     val pathInp = args(0)
     val cf = spark.read.option("inferSchema", true).option("recursiveFileLookup", true).json(pathInp)
     println("Number of documents = " + cf.count())
     import spark.implicits._
-    val df = cf.map { row =>
-      row.getAs[Seq[String]](0).flatMap(line => line.split("\\s+")).size
-    }.toDF("size")
+    val df = cf.map(row => row.getAs[String](0).split("""\s+""").size).toDF("size")
     val ef = df.agg(sum(col("size")))
     ef.show()
     spark.stop()
